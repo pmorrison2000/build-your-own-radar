5db2e9359156c3352f971e816bb8164122565f84
Peter Morrison <peter.morrison@se1.bp.com>
2021-05-16 17:12:42 +0800

    Make the Tags column mandatory

--- src/util/contentValidator.js
+++ src/util/contentValidator.js
@@ -23,7 +23,7 @@ const ContentValidator = function (columnNames) {
   self.verifyHeaders = function () {
 	//console.log('Column names:')
 	//console.log(columnNames)
-    _.each(['Technology', 'Horizon', 'Theme', 'Description', 'Status'], function (field) {
+    _.each(['Technology', 'Horizon', 'Theme', 'Description', 'Status', 'Tags'], function (field) {
       if (columnNames.indexOf(field) === -1) {
         throw new MalformedDataError(ExceptionMessages.MISSING_HEADERS)
       }
--- src/util/exceptionMessages.js
+++ src/util/exceptionMessages.js
@@ -1,7 +1,7 @@
 const ExceptionMessages = {
   TOO_MANY_RINGS: 'More than 4 rings.',
   MISSING_HEADERS: 'Document is missing one or more required headers or they are misspelled. ' +
-  'Check that your document contains headers for Technology, Horizon, Theme, Description, Status',
+  'Check that your document contains headers for Technology, Horizon, Theme, Description, Status, Tags',
   MISSING_CONTENT: 'Document is missing content.',
   SHEET_NOT_FOUND: 'Oops! We can’t find the Google Sheet you’ve entered. Can you check the URL?',
   UNAUTHORIZED: 'UNAUTHORIZED'
